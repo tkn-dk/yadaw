d7f0f3cad78d53856db89edd7cacb5521124c07b
Tage Korsdal Nielsen <tkn@korsdal.dk>
2023-07-22 13:53:01 +0200

    TrackNameColorDlg: Messed a little with the layout

--- src/dk/yadaw/widgets/TrackNameColorDlg.java
+++ src/dk/yadaw/widgets/TrackNameColorDlg.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class TrackNameColorDlg extends CenteredDialog {
@@ -18,7 +22,9 @@ public class TrackNameColorDlg extends CenteredDialog {
 
 	public TrackNameColorDlg(Frame owner, String currentName, Color currentColor, TrackNameColorListener updateListener ) {
 		super(owner, "Track name & Color");
-		setLayout( new FlowLayout() );
+		setLayout( new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
 		this.updateListener = updateListener;
 		
 		colorChooser = new TrackColorChooser( currentColor );
@@ -44,15 +50,58 @@ public class TrackNameColorDlg extends CenteredDialog {
 			}			
 		});
 		
+		JLabel trackNameLabel = new JLabel( "Enter track name:");
+		JLabel selectColorLabel = new JLabel( "Select track color:");
 		
-		add(trackNameEdit);
-		add(colorChooser);
-		add( okButton );
-		add( cancelButton );
+		Insets ins = new Insets( 0, 0, 20, 0 );
+		Insets defIns = gbc.insets;
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 3;
+		add( trackNameLabel, gbc );
+		
+		
+		gbc.insets = ins;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 3;
+		add(trackNameEdit, gbc);
+		
+		gbc.insets = defIns;
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 1;
+		add( selectColorLabel, gbc);
+		
+		gbc.insets = defIns;
+		gbc.insets = ins;
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 1;
+		add(colorChooser, gbc);
+
+		
+		gbc.insets = defIns;
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 1;
+		add( okButton, gbc );
+		
+		gbc.gridx = 2;
+		gbc.gridy = 4;
+		gbc.gridheight = 1;
+		gbc.gridwidth = 1;
+		add( cancelButton, gbc );
+		
 		setSize( 320, 280 );
 		locateToOwnerCenter();
 		setVisible(true);
-
 	}
 
 }
